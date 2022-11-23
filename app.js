2db56c7cd7a1538257df384013f5c398e6af96d8
midCenturyDude7 <mgriffes@gmail.com>
2022-11-22 22:11:55 -0800

    Updated styles and activated light/dark mode btn

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const navToggleBtn = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
 const footerBtnIcon = document.querySelector(".footer__btn-icon");
 const footerBtnText = document.querySelector(".footer__btn-text");
+const themeBtn = document.querySelector(".theme-btn");
+const navLinkThemeBtn = document.querySelector(".nav__link--theme-button");
 
 navToggleBtn.addEventListener("click", function handleMouseClick() {
   nav.classList.toggle("nav--visible");
@@ -18,3 +20,13 @@ function learnMoreBtn() {
 }
 
 learnMoreBtn();
+
+themeBtn.addEventListener("click", () => {
+  let element = document.body;
+  element.classList.toggle("light-mode");
+});
+
+navLinkThemeBtn.addEventListener("click", () => {
+  let element = document.body;
+  element.classList.toggle("light-mode");
+});
