8ac19d8794204d244e123a7f06e76fad626dcaf5
midCenturyDude7 <mgriffes@gmail.com>
2022-11-22 16:02:36 -0800

    Created helper function to style button based on mouseover/out

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const navToggleBtn = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
+const footerBtnIcon = document.querySelector(".footer__btn-icon");
+const footerBtnText = document.querySelector(".footer__btn-text");
 
-navToggleBtn.addEventListener("click", () => {
+navToggleBtn.addEventListener("click", function handleMouseClick() {
   nav.classList.toggle("nav--visible");
 });
+
+function learnMoreBtn() {
+  footerBtnIcon.addEventListener("mouseover", function handleMouseOver() {
+    footerBtnText.classList.toggle("footer__btn-text--hover");
+  });
+
+  footerBtnIcon.addEventListener("mouseout", function handleMouseOut() {
+    footerBtnText.classList = "footer__btn-text";
+  });
+}
+
+learnMoreBtn();
