34019fa161d4ec989bae565815cb19dce65adf4d
midCenturyDude7 <mgriffes@gmail.com>
2022-11-23 22:15:21 -0800

    Working on styles for larger page layout/design

--- app.js
+++ app.js
@@ -5,28 +5,48 @@ const footerBtnText = document.querySelector(".footer__btn-text");
 const themeBtn = document.querySelector(".theme-btn");
 const navLinkThemeBtn = document.querySelector(".nav__link--theme-button");
 
-navToggleBtn.addEventListener("click", function handleMouseClick() {
-  nav.classList.toggle("nav--visible");
-});
+// Mobile Navigation Menu Toggle Btn
+function mobileNavBtn() {
+  navToggleBtn.addEventListener("click", function handleMouseClick() {
+    nav.classList.toggle("nav--visible");
+  });
 
+  // Need to fix hamburger menu bug when drop down menu displays
+  // navToggleBtn.addEventListener("mouseout", function handleMouseOut() {
+  //   navToggleBtn.classList.toggle("nav-toggle--base");
+  // });
+}
+
+mobileNavBtn();
+
+// Learn More Btn
 function learnMoreBtn() {
+  window.onload = () => {
+    footerBtnText.style.color = "#050e1d";
+  };
+
   footerBtnIcon.addEventListener("mouseover", function handleMouseOver() {
-    footerBtnText.classList.toggle("footer__btn-text--hover");
+    footerBtnText.style.color = "#CCC";
   });
 
   footerBtnIcon.addEventListener("mouseout", function handleMouseOut() {
-    footerBtnText.classList = "footer__btn-text";
+    footerBtnText.style.color = "#050e1d";
   });
 }
 
 learnMoreBtn();
 
-themeBtn.addEventListener("click", () => {
-  let element = document.body;
-  element.classList.toggle("light-mode");
-});
+// Light/Dark Mode Btn
+function toggleLightMode() {
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
 
-navLinkThemeBtn.addEventListener("click", () => {
-  let element = document.body;
-  element.classList.toggle("light-mode");
-});
+  navLinkThemeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
+}
+
+toggleLightMode();
