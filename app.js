383ced64069f7a0ad89e53ede27855999dd25b73
midCenturyDude7 <mgriffes@gmail.com>
2022-12-31 11:20:20 -0800

    Added box with text-animation via css and js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// Reset animation
+"use strict";
+// Global DOM variables
+const colOne = document.querySelector(".one");
+const colTwo = document.querySelector(".two");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const paraOne = document.querySelector(".para-one");
+const paraTwo = document.querySelector(".para-two");
+
 // Mobile Navigation Menu Toggle Btn
 const mobileNavBtn = () => {
   const navToggleBtn = document.querySelector(".nav-toggle");
@@ -113,3 +123,45 @@ const downloadBtn = () => {
 };
 downloadBtn();
 
+// Home Page: col--right box with text animation
+// The 411
+first.addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault;
+    colOne.style.borderBottom = "4px solid var(--COLOR-GRAY-02)";
+    colTwo.style.borderBottom = "none";
+
+    // Reset animation helper: hides graph #2
+    paraTwo.classList.remove("para-two");
+    void paraTwo.offsetWidth;
+    paraTwo.classList.add("para-two");
+
+    // Reset animation helper: shows graph #1 w/animation
+    paraOne.classList.remove("para-one-invisible");
+    void paraOne.offsetWidth;
+    paraOne.classList.add("clicked-left");
+  },
+  false
+);
+
+// What's Next
+second.addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault;
+    colOne.style.borderBottom = "none";
+    colTwo.style.borderBottom = "4px solid var(--COLOR-GRAY-02)";
+
+    // Reset animation helper: hides graph #1
+    paraOne.classList.remove("para-one");
+    void paraOne.offsetWidth;
+    paraOne.classList.add("para-one-invisible");
+
+    // Reset animation helper: shows graph #2 w/animation
+    paraTwo.classList.remove("para-two");
+    void paraTwo.offsetWidth;
+    paraTwo.classList.add("clicked-right");
+  },
+  false
+);
