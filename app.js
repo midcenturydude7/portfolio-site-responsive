89bc384b97790933320e7766c842b417d3069f7c
midCenturyDude7 <mgriffes@gmail.com>
2023-02-13 21:26:32 -0800

    Working on setting up/activating contact form

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-// Reset animation
-("use strict");
-
 // Global DOM variables
 const iconBtn = document.querySelector(".icon-btn");
-const colOne = document.querySelector(".one");
-const colTwo = document.querySelector(".two");
-const first = document.getElementById("first");
-const second = document.getElementById("second");
-const paraOne = document.querySelector(".para-one");
-const paraTwo = document.querySelector(".para-two");
-const boxAnimLeft = document.querySelector(".box-animation-left");
-const boxAnimRight = document.querySelector(".box-animation-right");
 
 // Mobile Navigation Menu Toggle Btn
 const mobileNavBtn = () => {
@@ -108,98 +97,98 @@ function iconBtnMouseout() {
 }
 iconBtnMouseout();
 
-// Learn More Btn >> Large Screens
-const learnMoreBtnBigScreen = () => {
-  const mainBtnIcon = document.querySelector(".main__btn-icon");
+// // Learn More Btn >> Large Screens
+// const learnMoreBtnBigScreen = () => {
+//   const mainBtnIcon = document.querySelector(".main__btn-icon");
 
-  mainBtnIcon.addEventListener("mouseover", () => {
-    let element = document.getElementById("main__btn-text");
-    element?.classList.replace("main__btn-text", "main__btn-text--hover");
-  });
+//   mainBtnIcon.addEventListener("mouseover", () => {
+//     let element = document.getElementById("main__btn-text");
+//     element?.classList.replace("main__btn-text", "main__btn-text--hover");
+//   });
 
-  mainBtnIcon.addEventListener("mouseout", () => {
-    let element = document.getElementById("main__btn-text");
-    element?.classList.replace("main__btn-text--hover", "main__btn-text");
-  });
-};
-learnMoreBtnBigScreen();
+//   mainBtnIcon.addEventListener("mouseout", () => {
+//     let element = document.getElementById("main__btn-text");
+//     element?.classList.replace("main__btn-text--hover", "main__btn-text");
+//   });
+// };
+// learnMoreBtnBigScreen();
 
-const downloadBtn = () => {
-  const mainBtnIconRight = document.querySelector(".main__btn-icon--right");
+// const downloadBtn = () => {
+//   const mainBtnIconRight = document.querySelector(".main__btn-icon--right");
 
-  mainBtnIconRight.addEventListener("mouseover", () => {
-    let element = document.getElementById("main__btn-text--right");
-    element.classList.replace(
-      "main__btn-text--right",
-      "main__btn-text--right-hover"
-    );
-  });
+//   mainBtnIconRight.addEventListener("mouseover", () => {
+//     let element = document.getElementById("main__btn-text--right");
+//     element.classList.replace(
+//       "main__btn-text--right",
+//       "main__btn-text--right-hover"
+//     );
+//   });
 
-  mainBtnIconRight.addEventListener("mouseout", () => {
-    let element = document.getElementById("main__btn-text--right");
-    element.classList.replace(
-      "main__btn-text--right-hover",
-      "main__btn-text--right"
-    );
-  });
-};
-downloadBtn();
+//   mainBtnIconRight.addEventListener("mouseout", () => {
+//     let element = document.getElementById("main__btn-text--right");
+//     element.classList.replace(
+//       "main__btn-text--right-hover",
+//       "main__btn-text--right"
+//     );
+//   });
+// };
+// downloadBtn();
 
-// Home Page: col--right box with text animation
-// The 411
-first.addEventListener(
-  "click",
-  (e) => {
-    e.preventDefault;
-    // Border-bottom toggle with time interval delay to simulate slide
-    setTimeout(() => {
-      colOne.style.borderBottom = "4px solid var(--COLOR-GRAY-02)";
-      colTwo.style.borderBottom = "none";
-      boxAnimLeft.style.setProperty(
-        "--boxShadowAfter",
-        "5px 55px 55px 5px rgba(213, 206, 247, 0.185)"
-      );
-      boxAnimRight.style.setProperty("--boxShadowAfter", "none");
-    }, 110);
+// // Home Page: col--right box with text animation
+// // The 411
+// first.addEventListener(
+//   "click",
+//   (e) => {
+//     e.preventDefault;
+//     // Border-bottom toggle with time interval delay to simulate slide
+//     setTimeout(() => {
+//       colOne.style.borderBottom = "4px solid var(--COLOR-GRAY-02)";
+//       colTwo.style.borderBottom = "none";
+//       boxAnimLeft.style.setProperty(
+//         "--boxShadowAfter",
+//         "5px 55px 55px 5px rgba(213, 206, 247, 0.185)"
+//       );
+//       boxAnimRight.style.setProperty("--boxShadowAfter", "none");
+//     }, 110);
 
-    // Reset animation helper: hides graph #2
-    paraTwo.classList.remove("para-two");
-    void paraTwo.offsetWidth;
-    paraTwo.classList.add("para-two");
+//     // Reset animation helper: hides graph #2
+//     paraTwo.classList.remove("para-two");
+//     void paraTwo.offsetWidth;
+//     paraTwo.classList.add("para-two");
 
-    // Reset animation helper: shows graph #1 w/animation
-    paraOne.classList.remove("para-one-invisible");
-    void paraOne.offsetWidth;
-    paraOne.classList.add("clicked-left");
-  },
-  false
-);
+//     // Reset animation helper: shows graph #1 w/animation
+//     paraOne.classList.remove("para-one-invisible");
+//     void paraOne.offsetWidth;
+//     paraOne.classList.add("clicked-left");
+//   },
+//   false
+// );
 
-// What's Next
-second.addEventListener(
-  "click",
-  (e) => {
-    e.preventDefault;
-    // Border-bottom toggle with time interval delay ti simulate slide
-    setTimeout(() => {
-      colOne.style.borderBottom = "none";
-      colTwo.style.borderBottom = "4px solid var(--COLOR-GRAY-02)";
-      boxAnimRight.style.setProperty(
-        "--boxShadowAfter",
-        "5px 55px 55px 5px rgba(213, 206, 247, 0.185)"
-      );
-      boxAnimLeft.style.setProperty("--boxShadowAfter", "none");
-    }, 110);
+// // What's Next
+// second.addEventListener(
+//   "click",
+//   (e) => {
+//     e.preventDefault;
+//     // Border-bottom toggle with time interval delay ti simulate slide
+//     setTimeout(() => {
+//       colOne.style.borderBottom = "none";
+//       colTwo.style.borderBottom = "4px solid var(--COLOR-GRAY-02)";
+//       boxAnimRight.style.setProperty(
+//         "--boxShadowAfter",
+//         "5px 55px 55px 5px rgba(213, 206, 247, 0.185)"
+//       );
+//       boxAnimLeft.style.setProperty("--boxShadowAfter", "none");
+//     }, 110);
 
-    // Reset animation helper: hides graph #1
-    paraOne.classList.remove("para-one");
-    void paraOne.offsetWidth;
-    paraOne.classList.add("para-one-invisible");
+//     // Reset animation helper: hides graph #1
+//     paraOne.classList.remove("para-one");
+//     void paraOne.offsetWidth;
+//     paraOne.classList.add("para-one-invisible");
 
-    // Reset animation helper: shows graph #2 w/animation
-    paraTwo.classList.remove("para-two");
-    void paraTwo.offsetWidth;
-    paraTwo.classList.add("clicked-right");
-  },
-  false
-);
+//     // Reset animation helper: shows graph #2 w/animation
+//     paraTwo.classList.remove("para-two");
+//     void paraTwo.offsetWidth;
+//     paraTwo.classList.add("clicked-right");
+//   },
+//   false
+// );
--- email.js
+++ email.js
@@ -1,29 +1,37 @@
+/* eslint-disable no-undef */
 // import { password, credentials } from "./secureToken.js";
 
 // Contact form variables
-// const username = document.getElementById("name");
-// const email = document.getElementById("email");
-// const message = document.getElementById("info");
-const submit = document.getElementById("submit-btn");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("info");
+const submit = document.getElementById("form-contact");
 
 // Contact Form
 function submitForm() {
   submit.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("Submit Btn Clicked!");
-    // sendEmail();
+    sendEmail();
+    reset();
   });
 }
 submitForm();
 
-/* eslint-disable no-undef */
-// function sendEmail() {
-//   Email.send({
-//     Host: "smtp.elasticemail.com",
-//     Username: credentials,
-//     Password: password,
-//     To: credentials,
-//     From: email.value,
-//     Body: message.value,
-//   }).then(() => alert("Message sent successfully!"));
-// }
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "mgriffes@gmail.com",
+    Password: "97A8BC3C5171560F9583D4AE3DC5FA9EBA42",
+    To: "mgriffes@gmail.com",
+    From: email.value,
+    Body: message.value,
+  }).then(() => alert("Message sent successfully!"));
+  console.log(message.value);
+}
+
+function reset() {
+  username.value = "";
+  email.value = "";
+  message.value = "";
+}
